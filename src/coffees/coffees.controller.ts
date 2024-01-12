cfa0d168d76ecba9d5fda097572f956519948242
Muhammad Ali <35249585+MuhammadBnAly@users.noreply.github.com>
2024-01-12 02:21:49 +0200

    add route parameter to controller

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,9 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
     @Get('flavors')
     findAll(){
         return 'This action returns all coffees';
+    }
+    // @Get(':id')
+    // findOne(@Param() params){
+    //     return `This action returns ${params.id} coffees`;
+    // }
+    @Get('num/:id')
+    findTwo(@Param('id') id:string){
+        return `This action returns ${id} coffees`;
     }
 }
